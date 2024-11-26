e33419a3248b3b9d111454aa94877fcdd427823d
Michał Ćwikliński <michal@cwiklin.ski>
2024-11-26 18:23:48 +0100

    Added authentication initialization

--- service/auth/firebase/build.gradle.kts
+++ service/auth/firebase/build.gradle.kts
@@ -1,11 +1,13 @@
 import org.gradle.kotlin.dsl.android
 import org.gradle.kotlin.dsl.kotlin
+import org.jetbrains.compose.internal.utils.localPropertiesFile
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.buildconfig)
 }
 
 java {
@@ -41,6 +43,7 @@ kotlin {
         commonMain.dependencies {
             api(projects.common)
             api(projects.service.auth.api)
+            api(projects.service.storage.api)
             api(libs.google.firebase.common)
             api(libs.google.firebase.auth)
             api(libs.koin.compose)
@@ -63,4 +66,31 @@ android {
         sourceCompatibility = JavaVersion.toVersion(libs.versions.jdk.get().toInt())
         targetCompatibility = JavaVersion.toVersion(libs.versions.jdk.get().toInt())
     }
+}
+
+buildConfig {
+    className("FirebaseConfig")   // forces the class name. Defaults to 'BuildConfig'
+    packageName("mobi.cwiklinski.bloodline.config")
+    useKotlinOutput { internalVisibility = true }
+    val properties = localPropertiesFile.readLines().associate {
+        if (it.startsWith("#") || !it.contains("=")) return@associate "" to ""
+        val (key, value) = it.split("=", limit = 2)
+        key to value
+    }
+
+    val firebaseAndroidApiKey = properties["firebaseAndroidApiKey"].toString()
+    val firebaseIosApiKey = properties["firebaseIosApiKey"].toString()
+    val firebaseMessagingSenderId = properties["firebaseGcmSenderId"].toString()
+    val firebaseAppId = properties["firebaseApplicationId"].toString()
+    val firebaseStorageBucket = properties["firebaseStorageBucket"].toString()
+    val firebaseProjectId = properties["firebaseProjectId"].toString()
+    val firebaseDatabaseUrl = properties["firebaseDatabaseUrl"].toString()
+
+    buildConfigField("String", "FIREBASE_ANDROID_API_KEY", firebaseAndroidApiKey)
+    buildConfigField("String", "FIREBASE_IOS_API_KEY", firebaseIosApiKey)
+    buildConfigField("String", "FIREBASE_MESSAGING_SENDER_ID", firebaseMessagingSenderId)
+    buildConfigField("String", "FIREBASE_APP_ID", firebaseAppId)
+    buildConfigField("String", "FIREBASE_STORAGE_BUCKET", firebaseStorageBucket)
+    buildConfigField("String", "FIREBASE_PROJECT_ID", firebaseProjectId)
+    buildConfigField("String", "FIREBASE_DATABASE_URL", firebaseDatabaseUrl)
 }
---
+++ service/auth/firebase/src/desktopMain/kotlin/mobi/cwiklinski/bloodline/auth/firebase/AuthInitializerImpl.desktop.kt
@@ -0,0 +1,55 @@
+package mobi.cwiklinski.bloodline.auth.firebase
+
+import android.content.Context
+import com.google.firebase.FirebasePlatform
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.FirebaseOptions
+import dev.gitlive.firebase.initialize
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mobi.cwiklinski.bloodline.auth.api.AuthenticationInitializer
+import mobi.cwiklinski.bloodline.config.FirebaseConfig
+import mobi.cwiklinski.bloodline.storage.api.StorageService
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+actual class AuthenticationInitializerImpl actual constructor(
+    val storageService: StorageService,
+    val coroutineScope: CoroutineScope
+) : AuthenticationInitializer {
+
+    override fun run() {
+        FirebasePlatform.initializeFirebasePlatform(object : FirebasePlatform() {
+            override fun clear(key: String) {
+                coroutineScope.launch {
+                    storageService.clearAll()
+                }
+            }
+
+            override fun log(msg: String) {
+                println(msg)
+            }
+
+            override fun retrieve(key: String) = runBlocking {
+                storageService.getString(key, "")
+            }
+
+            override fun store(key: String, value: String) {
+                coroutineScope.launch {
+                    storageService.storeString(key, value)
+                }
+            }
+        })
+        Firebase.initialize(
+            Context(), options = FirebaseOptions(
+                applicationId = FirebaseConfig.FIREBASE_APP_ID,
+                apiKey = FirebaseConfig.FIREBASE_IOS_API_KEY,
+                databaseUrl = FirebaseConfig.FIREBASE_DATABASE_URL,
+                storageBucket = FirebaseConfig.FIREBASE_STORAGE_BUCKET,
+                projectId = FirebaseConfig.FIREBASE_PROJECT_ID,
+                gcmSenderId = FirebaseConfig.FIREBASE_MESSAGING_SENDER_ID
+            )
+        )
+    }
+}
