09692d8dd2e6e46680921ba8611a56267747ff2e
Michał Ćwikliński <michal@cwiklin.ski>
2025-02-13 14:42:36 +0100

    Notification service update and insert methods

--- shared/src/commonMain/kotlin/mobi/cwiklinski/bloodline/data/api/NotificationService.kt
+++ shared/src/commonMain/kotlin/mobi/cwiklinski/bloodline/data/api/NotificationService.kt
@@ -1,8 +1,34 @@
 package mobi.cwiklinski.bloodline.data.api
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import mobi.cwiklinski.bloodline.common.Either
 import mobi.cwiklinski.bloodline.domain.model.Notification
 
 interface NotificationService {
     fun getNotifications(): Flow<List<Notification>>
+
+    fun getNotification(id: String): Flow<Notification>
+
+    fun addNotification(
+        date: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
+        location: String = "Polska",
+        title: String = "",
+        message: String = "",
+        type: Int = 1
+    ): Flow<Either<Notification, Throwable>>
+
+    fun updateNotification(
+        id: String,
+        date: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
+        location: String = "Polska",
+        title: String = "",
+        message: String = "",
+        type: Int = 1
+    ): Flow<Either<Notification, Throwable>>
+
+    fun deleteNotification(id: String): Flow<Either<Boolean, Throwable>>
 }
--- shared/src/commonMain/kotlin/mobi/cwiklinski/bloodline/data/filed/NotificationServiceImplementation.kt
+++ shared/src/commonMain/kotlin/mobi/cwiklinski/bloodline/data/filed/NotificationServiceImplementation.kt
@@ -1,11 +1,73 @@
 package mobi.cwiklinski.bloodline.data.filed
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.datetime.LocalDate
+import mobi.cwiklinski.bloodline.common.Either
 import mobi.cwiklinski.bloodline.data.api.NotificationService
 import mobi.cwiklinski.bloodline.domain.model.Notification
 
 class NotificationServiceImplementation : NotificationService {
 
-    override fun getNotifications(): Flow<List<Notification>> = flowOf(DummyData.NOTIFICATIONS)
+    private val _memory = mutableListOf<Notification>()
+
+    init {
+        _memory.addAll(DummyData.NOTIFICATIONS)
+    }
+
+    override fun getNotifications(): Flow<List<Notification>> = flowOf(_memory)
+
+    override fun getNotification(id: String): Flow<Notification> = flowOf(_memory.first { it.id == id })
+
+    override fun addNotification(
+        date: LocalDate,
+        location: String,
+        title: String,
+        message: String,
+        type: Int
+    ) = flow<Either<Notification, Throwable>> {
+        val id = DummyData.generateString()
+        val notification = Notification(
+            id = id,
+            date = date,
+            type = type,
+            title = title,
+            message = message,
+            location = location
+        )
+        _memory.add(notification)
+        emit(Either.Left(notification))
+    }
+
+    override fun updateNotification(
+        id: String,
+        date: LocalDate,
+        location: String,
+        title: String,
+        message: String,
+        type: Int
+    ) = flow<Either<Notification, Throwable>> {
+        val notification = _memory.firstOrNull { it.id == id }
+        if (notification == null) {
+            emit(Either.Right(RuntimeException()))
+        } else {
+            val updatedNotification = Notification(
+                id = id,
+                date = date,
+                type = type,
+                title = title,
+                message = message,
+                location = location
+            )
+            _memory.removeAll { it.id == id }
+            _memory.add(updatedNotification)
+            emit(Either.Left(updatedNotification))
+        }
+    }
+
+    override fun deleteNotification(id: String) = flow<Either<Boolean, Throwable>> {
+        _memory.removeAll { it.id == id }
+        emit(Either.Left(true))
+    }
 }
