9d4b82e353d307143487d2f879aaa27c7c3dd6bc
Michał Ćwikliński <michal@cwiklin.ski>
2025-03-03 14:47:01 +0100

    Automatic navigational widget change for resolution

--- shared/src/commonMain/kotlin/mobi/cwiklinski/bloodline/ui/util/WindowUtil.kt
+++ shared/src/commonMain/kotlin/mobi/cwiklinski/bloodline/ui/util/WindowUtil.kt
@@ -1,11 +1,19 @@
 package mobi.cwiklinski.bloodline.ui.util
 
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.toSize
 import mobi.cwiklinski.bloodline.getOrientation
 import mobi.cwiklinski.bloodline.isMobile
 import mobi.cwiklinski.bloodline.isTablet
 import mobi.cwiklinski.bloodline.ui.widget.isHorizontal
 
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun RenderLayout(
     default: @Composable () -> Unit,
@@ -14,18 +22,38 @@ fun RenderLayout(
     phonePortrait: (@Composable () -> Unit)? = null,
     phoneLandscape: (@Composable () -> Unit)? = null
 ) {
+    val density = LocalDensity.current
+    val windowInfo = LocalWindowInfo.current
+    val size = with(density) { windowInfo.containerSize.toSize().toDpSize() }
+    val window =  WindowSizeClass.calculateFromSize(size)
     val isLandscape = getOrientation().isHorizontal()
     if (isMobile()) {
         if (isTablet()) {
-            tablet()
+            if (window.widthSizeClass == WindowWidthSizeClass.Expanded) {
+                desktop()
+            } else {
+                tablet()
+            }
         } else {
             if (isLandscape) {
-                phoneLandscape?.invoke() ?: default()
+                if (window.widthSizeClass == WindowWidthSizeClass.Compact) {
+                    phoneLandscape?.invoke() ?: default()
+                } else {
+                    tablet()
+                }
             } else {
-                phonePortrait?.invoke() ?: default()
+                if (window.widthSizeClass == WindowWidthSizeClass.Expanded) {
+                    tablet()
+                } else {
+                    phonePortrait?.invoke() ?: default()
+                }
             }
         }
     } else {
-        desktop()
+        if (window.widthSizeClass == WindowWidthSizeClass.Expanded) {
+            desktop()
+        } else {
+            tablet()
+        }
     }
 }
